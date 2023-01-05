b51d2318cbc404fc1d2aea44526bcae64d8df4bd
Auxilia <83941036+Auxilia-vinnarasi@users.noreply.github.com>
2023-01-05 12:04:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,12 @@ app.use("/api/buses",require("./routes/busesRoute"));
 app.use("/api/bookings",require("./routes/bookingsRoute"));
 
 //for deploying i have to tell heroku that where is client
+//i have to write config for heroku
 const path=require("path");
 if(process.env.NODE_ENV==="production")
 {
+    //i am deploying complete route folder so i have to write client/build
+    //for all req from the client im sending the response
     app.use(express.static("client/build"));
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client/build/index.html'));

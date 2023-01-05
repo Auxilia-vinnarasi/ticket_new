e2263138bfc23c0e51ed57c9dc41a0a2248fa3a5
Auxilia <83941036+Auxilia-vinnarasi@users.noreply.github.com>
2023-01-05 11:57:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express=require("express");
+const app=express();
+
 require("dotenv").config();
 const connectDB=require("./config/dbConfig");
 
-const app=express();
-
 connectDB();
 
+const port=process.env.port || 5000;
 app.use(express.json());
 
 app.use("/api/users",require("./routes/usersRoute"));
 app.use("/api/buses",require("./routes/busesRoute"));
 app.use("/api/bookings",require("./routes/bookingsRoute"));
 
-const port=process.env.port || 5000;
+//for deploying i have to tell heroku that where is client
+const path=require("path");
+if(process.env.NODE_ENV==="production")
+{
+    app.use(express.static("client/build"));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client/build/index.html'));
+    });
+}
+
+
 
 app.listen(port,()=>{
     console.log(`App is listening on port ${port}!`);
